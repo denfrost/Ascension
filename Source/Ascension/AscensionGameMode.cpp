cc53b541b38ebe15c580b7b1d840b41ca1c3b854
Shravan Kumar <kum_shravan@yahoo.com>
2017-03-10 21:59:20 +0530

    Created project, migrated the prototype project files.

---
+++ Source/Ascension/AscensionGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#include "Ascension.h"
+#include "AscensionGameMode.h"
+#include "AscensionCharacter.h"
+
+AAscensionGameMode::AAscensionGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Blueprints/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
